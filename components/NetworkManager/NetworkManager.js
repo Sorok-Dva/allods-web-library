2b5c8dc83ec967b5179d7970779c917b870395c4
Сорок два <garcia.llyam@gmail.com>
2020-03-15 21:22:21 +0100

    NetworkManager component

---
+++ components/NetworkManager/NetworkManager.js
@@ -0,0 +1,75 @@
+const BackError = Error;
+const request = require('request');
+
+let retrieveFullHost = (server) => {
+  switch (server) {
+    case 'Account':
+      if (!process.env.ACCOUNT_SERVER_HOST || !process.env.ACCOUNT_SERVER_PORT)
+        throw new BackError('Unable to reach config data, please verify your .env file.');
+      return { host: process.env.ACCOUNT_SERVER_HOST, port: process.env.ACCOUNT_SERVER_PORT };
+    default:
+      throw new BackError('Unknow server');
+  }
+};
+
+class NetworkManager {
+  constructor(server, endpoint) {
+    this.server = server;
+    this.endpoint = endpoint;
+    this.fullHost = retrieveFullHost(this.server);
+    this.reachable = false;
+    this.requestOptions = {
+      url: 'http://' + this.fullHost.host + ':' + this.fullHost.port,
+      method: 'GET'
+    };
+  }
+
+  resolveHost() {
+    return request(this.requestOptions, (error, response) => {
+      if (error) {
+        console.error('Unable to reach the server ' + JSON.stringify(this.fullHost));
+        return false;
+      }
+      this.reachable = true;
+    });
+  };
+
+  get(props) {
+    this.requestOptions.method = 'GET';
+    return this.execute(props);
+  }
+
+  post(props) {
+    this.requestOptions.method = 'POST';
+    return this.execute(props);
+  }
+
+  put(props) {
+    this.requestOptions.method = 'PUT';
+    return this.execute(props);
+  }
+
+  delete(props) {
+    this.requestOptions.method = 'DELETE';
+    return this.execute(props);
+  }
+
+  execute(data) {
+    if (!this.reachable) this.resolveHost();
+    return new Promise((resolve, reject) => {
+      let requestOptions = { ...this.requestOptions, json: data };
+      requestOptions.url += '/' + this.endpoint;
+
+      return request(requestOptions, (error, response) => {
+        if (error) return reject(error);
+        else return resolve(response.body);
+      });
+    });
+  }
+
+  changeEndpoint(endpoint) {
+    this.endpoint = endpoint;
+  }
+}
+
+module.exports = NetworkManager;
