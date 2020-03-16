481a8ad286eb98953a06444d7f81ceebc3d298a0
Сорок два <garcia.llyam@gmail.com>
2020-03-16 13:58:43 +0100

    add header licence

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*!
+ * allods-web-library
+ * Copyright(c) 2020 "Сорок два" <sorokdva.developer@gmail.com> (https://github.com/Sorok-Dva)
+ * MIT Licensed
+ */
 require('dotenv').config();
 
 module.exports = require('./components');
