4f459123f6028ad5b6dc13f994e0667d9641008a
Сорок два <garcia.llyam@gmail.com>
2020-03-16 13:01:53 +0100

    starting test + codecov

---
+++ test/NetworkManager.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chai = require('chai'),
+  expect = chai.expect,
+  { NetworkManager } = require('../index'),
+  request = new NetworkManager('Account');
+
+describe('# Network Manager', () => {
+  describe('Create a request for Account server', () => {
+    it('should returns a NetworkManager Entity with no endpoint', () => {
+      expect(request instanceof NetworkManager).to.be.equal(true);
+      expect(request.endpoint).to.be.equal(undefined);
+      request.changeEndpoint('accountDetails');
+    });
+
+    it('should returns a NetworkManager Entity with "accountDetails" endpoint', () => {
+      return expect(request.endpoint).to.be.equal('accountDetails');
+    });
+  });
+});
